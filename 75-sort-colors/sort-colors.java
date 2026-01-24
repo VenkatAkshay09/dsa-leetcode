3c43954c315ef36123222c468192742d49825379
Venkat Akshay S <167801383+VenkatAkshay09@users.noreply.github.com>
2026-01-24 11:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (33.12%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int a =0, b=0,c=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==0) a++;
+            else if(nums[i]==1) b++;
+            else c++;
+        }
+        for(int i=0; i <nums.length; i++){
+            if(i<a) nums[i]=0;
+            else if(i>a-1 && i<nums.length-c) nums[i]=1;
+            else nums[i]=2;
+        }
+        
+    }
+}
