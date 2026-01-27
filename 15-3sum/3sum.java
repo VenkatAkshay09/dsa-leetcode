5d0befd94a22a9bdf82604276cea82753421c71c
Venkat Akshay S <167801383+VenkatAkshay09@users.noreply.github.com>
2026-01-27 15:10:23 +0530

    Time: 36 ms (32.90%) | Memory: 59.5 MB (31.11%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> resList = new ArrayList<>();
-        for (int i = 0; i < nums.length-2 && nums[i] <= 0; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            // two pointer way
-            int l = i + 1;
-            for (int r = nums.length - 1; l < r;) {
-                if (nums[i] + nums[l] + nums[r] == 0) {
-                    resList.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1])
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0; i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l = i+1;
+            int r = nums.length-1;
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    if(l>i+1 && nums[l]==nums[l-1]){
                         l++;
-                    while (r > l && nums[r] == nums[r + 1])
+                        continue;
+                    }
+                    if(r<nums.length-1 && nums[r]==nums[r+1]){
                         r--;
-                } else if (nums[i] < -1 * (nums[l] + nums[r])) {
+                        continue;
+                    }
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
                     l++;
-                } else
                     r--;
-            }
-
+                }
+                else if(sum<0) l++;
+                else r--;
+            }        
         }
-        return resList;
+        return res;
     }
 }
