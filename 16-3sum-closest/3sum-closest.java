3a94ba62596836966482226cf8dc47792642b19a
Venkat Akshay S <167801383+VenkatAkshay09@users.noreply.github.com>
2026-01-27 18:15:50 +0530

    Time: 16 ms (90.77%) | Memory: 45.4 MB (85.34%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,42 +1,33 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int sum=0;
-        int sol = 0;
-        int maxDiff = Integer.MAX_VALUE;
-        int len = nums.length;
-        for(int i=0; i<len-2; i++){
-            if(i>0 && nums[i]==nums[i-1]){
+        int diff = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            }
-            int l=i+1;
-            for(int r=len-1; r<len && l<r; ){
-                sum = nums[i]+nums[l]+nums[r];
-                if(sum==target)
-                    return sum;
-                else if(sum<target){
-                    int diff = Math.abs(target-sum);
-                    if(diff<maxDiff){
-                        maxDiff = diff;
-                        sol = sum;
-                        l++;
+            int l = i + 1;
+            int r = nums.length - 1;
+            
+            while (l < r) {
+                int s = nums[i] + nums[l] + nums[r];
+            int d = Math.abs(s - target);
+                if (s == target)
+                    return s;
+                else {
+                    if (d < diff) {
+                        diff = d;
+                        sum = s;
                     }
-                    else
+                    if (s < target)
                         l++;
-                }
-                else{
-                    int diff = Math.abs(target-sum);
-                    if(diff<maxDiff){
-                        maxDiff = diff;
-                        sol = sum;
-                        r--;
-                    }
                     else
                         r--;
                 }
             }
+
         }
-        return sol;
-        
+        return sum;
+
     }
 }
