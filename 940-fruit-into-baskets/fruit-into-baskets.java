cf72ee25a19589d5f1b308e9df62af38e222f39d
Venkat Akshay S <167801383+VenkatAkshay09@users.noreply.github.com>
2026-02-02 18:24:57 +0530

    Time: 54 ms (64.43%) | Memory: 70.8 MB (18.86%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,26 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int fruitType = 0; 
-        int low=0;
-        int count = Integer.MIN_VALUE;
-        Map<Integer,Integer> basket = new HashMap<>();
+        int low = 0, max = 0;
+        Map<Integer, Integer> basket = new HashMap<>();
+
         for (int high = 0; high < fruits.length; high++) {
-            if(basket.containsKey(fruits[high])) 
-                basket.put(fruits[high],basket.get(fruits[high])+1);
-            else {
-                basket.put(fruits[high],1);
-                fruitType++;
-            }
-            while(fruitType>2){
-                basket.put(fruits[low], basket.get(fruits[low])-1);
-                if(basket.get(fruits[low])==0){
+            basket.put(fruits[high], basket.getOrDefault(fruits[high], 0) + 1);
+
+            while (basket.size() > 2) {
+                basket.put(fruits[low], basket.get(fruits[low]) - 1);
+                if (basket.get(fruits[low]) == 0) {
                     basket.remove(fruits[low]);
-                    fruitType--;
                 }
                 low++;
             }
-            if(fruitType<=2) count= Math.max(count, high-low+1);
+
+            max = Math.max(max, high - low + 1);
         }
-        return count;
+        return max;
     }
 }
